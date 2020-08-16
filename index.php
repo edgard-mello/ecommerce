ef567ae96747a91f246bb6b0c8c74cc5984e7503
Edgard Mello <edggard@gmail.com>
2020-08-16 16:59:01 -0300

    Roteamento inicial

--- index.php
+++ index.php
@@ -2,18 +2,23 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
+
+    $page = new Page();
+
+    $page->setTpl("index");
+
 	//echo "OK";
-	$sql = new Hcode\DB\Sql();
-
-    $result = $sql->select("SELECT * FROM tb_users");
-
-    echo json_encode($result);
+	//$sql = new Hcode\DB\Sql();
+    //$result = $sql->select("SELECT * FROM tb_users");
+    //echo json_encode($result);
 
 });
 
