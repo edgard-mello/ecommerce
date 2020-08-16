2de6d22ddfb2fcfa48267010dc8e0917b880a04a
Edgard Mello <edggard@gmail.com>
2020-08-16 18:35:58 -0300

    Criação da área admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
@@ -20,6 +21,13 @@ $app->get('/', function() {
     //$result = $sql->select("SELECT * FROM tb_users");
     //echo json_encode($result);
 
+});
+
+$app->get('/admin', function(){
+
+	$page = new PageAdmin();
+
+	$page-> setTpl("index");
 });
 
 $app->run();
